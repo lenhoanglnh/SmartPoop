0deeff476133248b506f370dde1b81a506e4f439
Le Nguyen Hoang <len.hoang.lnh@gmail.com>
2021-10-20 12:07:46 +0200

    Added a file to compile both epub and pdf

---
+++ French/compile.sh
@@ -0,0 +1,21 @@
+pandoc --toc -o SmartPoop-fr.epub titre.txt \
+  README.md \
+  1-L\'or.md \
+  2-Filtrer.md \
+  3-Biais.md \
+  4-Fuites.md \
+  5-FakePoops.md \
+  6-Corruption.md \
+  7-Psy.md \
+  8-Trone.md
+
+pandoc --toc --template=template.tex -o SmartPoop-fr.pdf titre.txt \
+  README.md \
+  1-L\'or.md \
+  2-Filtrer.md \
+  3-Biais.md \
+  4-Fuites.md \
+  5-FakePoops.md \
+  6-Corruption.md \
+  7-Psy.md \
+  8-Trone.md
